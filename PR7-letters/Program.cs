10f829d50493d79482461a7875b71f0005fb007c
aiko <mildxw@gmail.com>
2026-05-06 19:14:44 +0300

    ПР7 Ч1 Буквы

---
+++ PR7-letters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/// <summary>
+/// Основной класс программы.
+/// </summary>
+class ArrayExample
+{
+    /// <summary>
+    /// Точка входа в программу.
+    /// </summary>
+    static void Main()
+    {
+        char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+        string name = "";
+        int[] a = new int[10];
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
+        }
+
+        Console.ReadKey();
+    }
+
+    /// <summary>
+    /// Выводит сообщение в консоль.
+    /// </summary>
+    /// <param name="name">Имя пользователя.</param>
+    /// <param name="msg">Номер сообщения.</param>
+    static void SendMessage(string name, int msg)
+    {
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
+    }
+}
